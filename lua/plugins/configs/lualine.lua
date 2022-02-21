b19d56af4dc7d9a0b26dce95439a4ddb8f893aa4
Trần Đức Nam <dynamo.foss@gmail.com>
2022-02-22 00:17:55 +0700

    chore: change sections configuration of lualine

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -11,4 +11,24 @@ lualine.setup({
             'NvimTree',
         },
     },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = {
+            {
+                'filename',
+                path = 1, -- Relative path
+            },
+        },
+        lualine_x = { 'filetype' },
+        lualine_y = {
+            {
+                'encoding',
+                separator = { left = '', right = '' }, -- Not use powerline between encoding and file format
+            },
+            'fileformat',
+        },
+        lualine_z = { 'progress', 'location' },
+    },
+    extensions = { 'quickfix' },
 })
