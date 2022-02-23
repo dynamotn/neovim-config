86bc2f8172c2a6ec1db3c2207935077624f16e0d
Trần Đức Nam <dynamo.foss@gmail.com>
2022-02-23 22:41:41 +0700

    refactor: add keymaps config and revert folder name of options

--- lua/plugins/register.lua
+++ lua/plugins/register.lua
@@ -6,10 +6,11 @@ local M = {}
 
 -- Register plugin config
 M.register_config = function(name, config_function)
-    local result = "pcall(require, 'plugins.configs." .. name .. "')"
+    local result = "pcall(require, 'plugins.options." .. name .. "')"
     if type(config_function) == 'string' and config_function ~= '' then
         result = result .. '.' .. config_function .. '()'
     end
+    pcall(require, 'plugins.keymaps.' .. name)
     return result
 end
 
