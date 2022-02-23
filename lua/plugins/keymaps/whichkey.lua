9feed665a39cdc97bede480f1445f3b9dc9a6a21
Trần Đức Nam <dynamo.foss@gmail.com>
2022-02-23 23:43:08 +0700

    feat: add basic main trigger keymaps

---
+++ lua/plugins/keymaps/whichkey.lua
@@ -0,0 +1,15 @@
+return {
+    -- Space key
+    ['<space>'] = { name = 'Dynamo Trigger' },
+    ['<space>b'] = { name = 'Buffer' },
+    ['<space>d'] = { name = 'Debug / Quick fix / Test' },
+    ['<space>f'] = { name = 'File' },
+    ['<space>p'] = { name = 'Project' },
+    ['<space>s'] = { name = 'Search' },
+    ['<space>l'] = { name = 'Language tool' },
+    ['<space>t'] = { name = 'Tab' },
+    ['<space>x'] = { name = 'Text manipulation' },
+
+    -- Leader key
+    ['<leader>'] = { name = 'Secondary Trigger' },
+}
--- lua/plugins/register.lua
+++ lua/plugins/register.lua
@@ -6,12 +6,33 @@ local M = {}
 
 -- Register plugin config
 M.register_config = function(name, config_function)
+    M.register_keymaps(name)
+    return M.register_options(name)
+end
+
+-- Load per-plugin options to setup plugin
+M.register_options = function(name, config_function)
     local result = "pcall(require, 'plugins.options." .. name .. "')"
     if type(config_function) == 'string' and config_function ~= '' then
         result = result .. '.' .. config_function .. '()'
     end
-    pcall(require, 'plugins.keymaps.' .. name)
+
     return result
+end
+
+-- Load per-plugin keymaps by `whichkey` plugin
+M.register_keymaps = function(name)
+    local present, whichkey = pcall(require, 'which-key')
+    if not present then
+        return
+    end
+
+    local present, keymaps = pcall(require, 'plugins.keymaps.' .. name)
+    if not present or not keymaps then
+        return
+    end
+
+    whichkey.register(keymaps)
 end
 
 return M
