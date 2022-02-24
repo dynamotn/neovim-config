5a422a0e5e823256821235af1b4ca7c3043b75a3
Trần Đức Nam <dynamo.foss@gmail.com>
2022-02-24 22:46:00 +0700

    feat: intelligently reopen file at last edit position

---
+++ lua/plugins/options/lastplace.lua
@@ -0,0 +1,11 @@
+local present, lastplace = pcall(require, 'nvim-lastplace')
+
+if not present then
+    return
+end
+
+lastplace.setup({
+    lastplace_ignore_buftype = { 'quickfix', 'nofile', 'help' },
+    lastplace_ignore_filetype = { 'gitcommit', 'gitrebase' },
+    lastplace_open_folds = true,
+})
