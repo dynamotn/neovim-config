a3393d3de325a64a2f4e45209db7b962833702c6
Trần Đức Nam <dynamo.foss@gmail.com>
2022-03-10 23:11:00 +0700

    feat: customize status line to show TS, LSP and change icon of git diff

--- lua/plugins/options/lualine.lua
+++ lua/plugins/options/lualine.lua
@@ -15,7 +15,14 @@ lualine.setup({
     },
     sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_b = {
+            'branch',
+            {
+                'diff',
+                symbols = { added = ' ', modified = ' ', removed = ' ' },
+            },
+            'diagnostics',
+        },
         lualine_c = {
             {
                 'filename',
@@ -26,7 +33,40 @@ lualine.setup({
                 cond = gps.is_available,
             },
         },
-        lualine_x = { 'filetype' },
+        lualine_x = {
+            {
+                function()
+                    local b = vim.api.nvim_get_current_buf()
+                    if next(vim.treesitter.highlighter.active[b]) then
+                        return ' '
+                    end
+                    return ''
+                end,
+                color = { fg = '#98be65' },
+            },
+            {
+                function(msg)
+                    msg = msg or '  Inactive'
+                    local buf_clients = vim.lsp.buf_get_clients()
+                    if next(buf_clients) == nil then
+                        -- TODO: clean up this if statement
+                        if type(msg) == 'boolean' or #msg == 0 then
+                            return '  Inactive'
+                        end
+                        return msg
+                    end
+                    local buf_client_names = {}
+
+                    -- add client
+                    for _, client in pairs(buf_clients) do
+                        table.insert(buf_client_names, client.name)
+                    end
+                    return '  ' .. table.concat(buf_client_names, ', ')
+                end,
+                color = { fg = '#008080', gui = 'bold' },
+            },
+            'filetype',
+        },
         lualine_y = {
             {
                 'encoding',
