1fa860fbe031c1503535320ffd4ff6b2e0133936
Trần Đức Nam <dynamo.foss@gmail.com>
2022-03-11 00:48:08 +0700

    fix: typo

--- lua/plugins/keymaps/telescope.lua
+++ lua/plugins/keymaps/telescope.lua
@@ -1,6 +1,6 @@
 return {
     ['<Space>fg'] = { '<cmd>Telescope live_grep<CR>', 'Grep file' },
-    ['<Space>fq'] = { '<cmd>Telescope find_files<CR>', 'File file' },
+    ['<Space>fq'] = { '<cmd>Telescope find_files<CR>', 'Find file' },
 
     ['<Space>bq'] = { '<cmd>Telescope buffers<CR>', 'Find buffers' },
 }
