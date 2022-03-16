2baaf6dce83ae30a1a44a21c7ad857c6aa82b4d7
Trần Đức Nam <dynamo.foss@gmail.com>
2022-03-16 23:16:46 +0700

    feat: show LSP diagnostics and switch to tabpages mode in bufferline

--- lua/plugins/options/bufferline.lua
+++ lua/plugins/options/bufferline.lua
@@ -6,9 +6,11 @@ end
 
 bufferline.setup({
     options = {
+        mode = 'tabs',
         numbers = function(opts)
             return string.format('%s', opts.raise(opts.id))
         end,
+        diagnostics = 'nvim_lsp',
         show_buffer_close_icons = false,
         show_close_icon = false,
         separator_style = 'slant',
