45c2ab95a8193179a811c54fa09085d936210a5d
Trần Đức Nam <dynamo.foss@gmail.com>
2022-04-12 01:36:15 +0700

    feat: use shellcheck, shfmt and shellharden with Bash

---
+++ lua/languages/bash/null_ls.lua
@@ -0,0 +1,11 @@
+return function(null_ls, filetypes)
+    return {
+        null_ls.builtins.code_actions.shellcheck.with({ filetypes = filetypes }),
+        null_ls.builtins.diagnostics.shellcheck.with({ filetypes = filetypes }),
+        null_ls.builtins.formatting.shfmt.with({
+            filetypes = filetypes,
+            extra_args = { '-i', '2', '-ci', '-bn', '-sr' },
+        }),
+        null_ls.builtins.formatting.shellharden.with({ filetypes = filetypes }),
+    }
+end
