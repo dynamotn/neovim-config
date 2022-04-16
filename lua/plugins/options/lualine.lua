ff2698f40164d5a4bfacf4e7b0b7efa98bfaea90
Trần Đức Nam <dynamo.foss@gmail.com>
2022-04-17 00:17:34 +0700

    refactor: fix local variable

--- lua/plugins/options/lualine.lua
+++ lua/plugins/options/lualine.lua
@@ -71,7 +71,7 @@ lualine.setup({
             },
             {
                 function()
-                    msg = ' '
+                    local msg = ' '
                     for _, tool in pairs(require('languages').get_tools_by_filetype(vim.bo.filetype)) do
                         if vim.fn.executable(tool) == 1 then
                             msg = msg .. tool .. ' '
