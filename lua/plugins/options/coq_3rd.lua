9257083ae7ec5e62b5ee8d58b4bf2693d6c5f3ad
Trần Đức Nam <dynamo.foss@gmail.com>
2022-04-16 21:19:12 +0700

    feat: add Github Copilot completion

--- lua/plugins/options/coq_3rd.lua
+++ lua/plugins/options/coq_3rd.lua
@@ -8,5 +8,6 @@ coq_3rd({
     { src = 'repl', short_name = 'REPL' },
     { src = 'nvimlua', short_name = 'NVIM', conf_only = false },
     { src = 'bc', short_name = 'MATH' },
+    { src = 'copilot', short_name = 'COP', accept_key = '<c-f>' },
     { src = 'orgmode', short_name = 'ORG' },
 })
