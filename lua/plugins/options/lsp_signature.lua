7d74e09c352288d1202d27b3206c27bd7ee14d88
Trần Đức Nam <dynamo.foss@gmail.com>
2022-04-17 23:46:05 +0700

    feat: change ui decorate of signature

--- lua/plugins/options/lsp_signature.lua
+++ lua/plugins/options/lsp_signature.lua
@@ -4,4 +4,11 @@ if not present then
     return
 end
 
-lsp_signature.setup({})
+lsp_signature.setup({
+    bind = true,
+    handler_opts = {
+        border = 'rounded',
+    },
+    zindex = 50,
+    check_completion_visible = true,
+})
