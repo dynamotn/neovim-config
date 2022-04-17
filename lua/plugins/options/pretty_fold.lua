13af823ced23ee3a54312be8d92db9a4c8be0b53
Trần Đức Nam <dynamo.foss@gmail.com>
2022-04-18 00:18:18 +0700

    feat: customize fold text

--- lua/plugins/options/pretty_fold.lua
+++ lua/plugins/options/pretty_fold.lua
@@ -4,5 +4,26 @@ if not present then
     return
 end
 
-pretty_fold.setup({})
-require('pretty-fold.preview').setup()
+pretty_fold.setup({
+    keep_indentation = false,
+    fill_char = '━',
+    sections = {
+        left = {
+            '━ ',
+            function()
+                return string.rep('*', vim.v.foldlevel)
+            end,
+            ' ━┫',
+            'content',
+            '┣',
+        },
+        right = {
+            '┫ ',
+            'number_of_folded_lines',
+            ' ┣━━',
+        },
+    },
+})
+require('pretty-fold.preview').setup({
+    border = 'rounded',
+})
