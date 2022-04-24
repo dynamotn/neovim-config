a28475c396affea44bf5df5e3fdaf0922c6ba23d
Trần Đức Nam <dynamo.foss@gmail.com>
2022-04-24 17:52:34 +0700

    feat: add gitsign code action

--- lua/plugins/options/null_ls.lua
+++ lua/plugins/options/null_ls.lua
@@ -13,6 +13,7 @@ if present then
     table.insert(sources, null_ls.builtins.diagnostics.trail_space)
     table.insert(sources, null_ls.builtins.formatting.trim_whitespace)
     table.insert(sources, null_ls.builtins.formatting.trim_newlines)
+    table.insert(sources, null_ls.builtins.code_actions.gitsigns)
 
     null_ls.setup({
         sources = sources,
