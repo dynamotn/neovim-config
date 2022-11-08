59d4a21b2381517948ff0b62ebcedf43d3bf7767
Trần Đức Nam <dynamo.foss@gmail.com>
2022-11-09 00:22:18 +0700

    feat: bash DAP

--- lua/languages/dapconfig.lua
+++ lua/languages/dapconfig.lua
@@ -14,4 +14,30 @@ M.neovim = {
   },
 }
 
+local BASHDB_DIR = require('mason-registry').get_package('bash-debug-adapter'):get_install_path()
+  .. '/extension/bashdb_dir'
+M.bash = {
+  mason_install = true,
+  adapter = {
+    type = 'executable',
+    command = 'bash-debug-adapter',
+  },
+  configurations = {
+    {
+      type = 'bash',
+      request = 'launch',
+      name = 'Bash: Launch file',
+      program = '${file}',
+      cwd = '${fileDirname}',
+      pathBashdb = BASHDB_DIR .. '/bashdb',
+      pathBashdbLib = BASHDB_DIR,
+      pathBash = 'bash',
+      pathCat = 'cat',
+      pathMkfifo = 'mkfifo',
+      pathPkill = 'pkill',
+      env = {},
+      args = {},
+    },
+  },
+}
 return M
