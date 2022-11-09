d7bf27ccf98de6f520a9566a14806e18b42d3e54
Trần Đức Nam <dynamo.foss@gmail.com>
2022-11-09 11:31:52 +0700

    feat: add some keymaps for DAP

--- lua/plugins/keymaps/dap.lua
+++ lua/plugins/keymaps/dap.lua
@@ -1,4 +1,7 @@
 return {
   ['<Space>qb'] = { dynamo_cmdcr('lua require("dap").toggle_breakpoint()'), 'Mark or delete breakpoints' },
   ['<Space>qz'] = { dynamo_cmdcr('lua require("dap").clear_breakpoints()'), 'Clear all breakpoints' },
+  ['<Space>qd'] = { dynamo_cmdcr('lua require("dap").continue()'), 'Launch debug / Resume execution' },
+  ['<F6>'] = { dynamo_cmdcr('lua require("dap").continue()'), 'Launch debug / Resume execution' },
+  ['<Space>qo'] = { dynamo_cmdcr('lua require("dap").repl.open()'), 'Open inspecting the state via the built-in REPL' },
 }
