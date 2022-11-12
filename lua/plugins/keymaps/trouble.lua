14033b1d61546b08169a32b838e00c4a347c0d1a
Trần Đức Nam <dynamo.foss@gmail.com>
2022-11-12 23:13:40 +0700

    feat: add quick keymaps for diagnostic

--- lua/plugins/keymaps/trouble.lua
+++ lua/plugins/keymaps/trouble.lua
@@ -1,6 +1,7 @@
 return {
   ['<Space>ed'] = { dynamo_cmdcr('TroubleToggle document_diagnostics'), 'Show diagnostic of document' },
   ['<Space>ew'] = { dynamo_cmdcr('TroubleToggle workspace_diagnostics'), 'Show diagnostic of workspace' },
+  ['<F4>'] = { dynamo_cmdcr('TroubleToggle workspace_diagnostics'), 'Show diagnostic of workspace' },
   ['<Space>el'] = { dynamo_cmdcr('TroubleToggle loclist'), 'Show loclist' },
   ['<Space>eq'] = { dynamo_cmdcr('TroubleToggle quickfix'), 'Show quickfix' },
 }
