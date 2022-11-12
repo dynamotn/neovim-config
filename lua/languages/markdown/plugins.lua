592a34c4b6453108d09f203c4135b85a7b3cfa81
Trần Đức Nam <dynamo.foss@gmail.com>
2022-11-13 00:57:16 +0700

    temp

--- lua/languages/markdown/plugins.lua
+++ lua/languages/markdown/plugins.lua
@@ -1,11 +1,11 @@
-return function(register_config)
+return function(register_config, filetypes)
   return {
     { -- Preview markdown
       'iamcco/markdown-preview.nvim',
       run = function()
         vim.fn['mkdp#util#install']()
       end,
-      config = register_config('markdown_preview'),
+      config = register_config('markdown_preview', nil, filetypes),
     },
   }
 end
