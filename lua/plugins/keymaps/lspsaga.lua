17eb3b5401be8669caecff8e0054cdb1cbaa98b1
Trần Đức Nam <dynamo.foss@gmail.com>
2022-11-13 10:55:36 +0700

    refactor: remove conflict keymaps

--- lua/plugins/keymaps/lspsaga.lua
+++ lua/plugins/keymaps/lspsaga.lua
@@ -3,14 +3,6 @@ return function(whichkey, bufnr)
     ['gd'] = { dynamo_cmdcr('Lspsaga preview_definition'), 'Preview definition' },
     ['gh'] = { dynamo_cmdcr('Lspsaga lsp_finder'), 'Show definition and references' },
     ['K'] = { dynamo_cmdcr('Lspsaga hover_doc'), 'Hover document' },
-    ['<C-u>'] = {
-      dynamo_cmdcr('lua require("lspsaga.action").smart_scroll_with_saga(-1)'),
-      'Scroll up in preview window',
-    },
-    ['<C-d>'] = {
-      dynamo_cmdcr('lua require("lspsaga.action").smart_scroll_with_saga(1)'),
-      'Scroll down in preview window',
-    },
     ['<leader>k'] = { dynamo_cmdcr('Lspsaga signature_help'), 'Signature help' },
     ['<Space>lr'] = { dynamo_cmdcr('Lspsaga rename'), 'Rename' },
     ['<Space>la'] = { dynamo_cmdcr('Lspsaga code_action'), 'Code Action' },
