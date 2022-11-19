04d5019c6e07abfdf8621f416a100514ce5fa7f7
Trần Đức Nam <dynamo.foss@gmail.com>
2022-11-19 11:04:55 +0700

    refactor: F1 command

--- lua/plugins/keymaps/neotree.lua
+++ lua/plugins/keymaps/neotree.lua
@@ -1,5 +1,5 @@
 return {
-  ['<F1>'] = { dynamo_cmdcr('Neotree'), 'File explorer' },
+  ['<F1>'] = { dynamo_cmdcr('Neotree toggle'), 'File explorer' },
   ['<Space>ft'] = { dynamo_cmdcr('Neotree'), 'File explorer' },
   ['<Space>gt'] = { dynamo_cmdcr('Neotree source=git_status'), 'Status explorer' },
 }
