2ef808e429cce347ad401096d3636c0db7eedcea
Trần Đức Nam <dynamo.foss@gmail.com>
2022-12-10 16:15:34 +0700

    refactor: add keymap to terminate DAP and change ui of DAP

--- lua/plugins/keymaps/dap.lua
+++ lua/plugins/keymaps/dap.lua
@@ -3,5 +3,6 @@ return {
   ['<leader>dz'] = { dynamo_cmdcr('lua require("dap").clear_breakpoints()'), 'Clear all breakpoints' },
   ['<leader>dd'] = { dynamo_cmdcr('lua require("dap").continue()'), 'Launch debug / Resume execution' },
   ['<F6>'] = { dynamo_cmdcr('lua require("dap").continue()'), 'Launch debug / Resume execution' },
+  ['<leader>dt'] = { dynamo_cmdcr('lua require("dap").terminate()'), 'Terminate debug' },
   ['<leader>do'] = { dynamo_cmdcr('lua require("dap").repl.open()'), 'Open inspecting the state via the built-in REPL' },
 }
