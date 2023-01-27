0de1b7a7c73fc470e3fb28c28e84a4a11da32671
Trần Đức Nam <dynamo.foss@gmail.com>
2023-01-28 00:55:26 +0700

    refactor: correct dependency of yaml_companion

--- lua/languages/yaml/plugins.lua
+++ lua/languages/yaml/plugins.lua
@@ -1,3 +1,10 @@
 return {
-  { 'someone-stole-my-name/yaml-companion.nvim', name = 'yaml_companion' }, -- Autodetect YAML schema
+  { -- Autodetect YAML schema
+    'someone-stole-my-name/yaml-companion.nvim',
+    name = 'yaml_companion',
+    dependencies = {
+      'lsp',
+      'plenary',
+    },
+  },
 }
