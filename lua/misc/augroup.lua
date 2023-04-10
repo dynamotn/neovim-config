92b470eded799a235b79f249f40e0ac2c4c74a7d
Trần Đức Nam <dynamo.foss@gmail.com>
2023-04-11 00:25:05 +0700

    refactor: use native api for augroup & autocmd

--- lua/misc/augroup.lua
+++ lua/misc/augroup.lua
@@ -1,162 +1,133 @@
 ---------------------------------------
 --      Automatic Group setting      --
 ---------------------------------------
-local ex_cmd = vim.api.nvim_command -- Execute Vim ex commands
 local M = {}
 
-M.create_augroups = function(definitions, is_buffer, client_id)
-  for group_name, definition in pairs(definitions) do
-    if client_id then
-      ex_cmd('augroup ' .. group_name .. client_id)
-    else
-      ex_cmd('augroup ' .. group_name)
-    end
-    if is_buffer then
-      ex_cmd('autocmd! * <buffer>')
-    else
-      ex_cmd('autocmd!')
-    end
-    for _, def in ipairs(definition) do
-      local command = table.concat(vim.tbl_flatten({ 'autocmd', def }), ' ')
-      ex_cmd(command)
-    end
-    ex_cmd('augroup END')
-  end
-end
-
-M.delete_augroups = function(name)
-  vim.schedule(function()
-    if vim.fn.exists('#' .. name) == 1 then
-      vim.cmd('augroup ' .. name)
-      vim.cmd('autocmd!')
-      vim.cmd('augroup END')
-    end
-  end)
-end
-
 M.load_default_augroups = function()
-  M.create_augroups({
-    mkdir_saving = {
-      { 'BufWritePre', '*', 'lua dynamo_mkdir_saving()' },
-    },
-    terminal = {
-      { 'BufEnter', '*', 'lua dynamo_setft_terminal()' },
-    },
-    quick_quit_manual = {
-      {
-        'FileType',
-        'qf,help,man,lspinfo,lsp-installer',
-        'nnoremap <silent> <buffer> q :close<CR>',
-      },
-    },
-    lastplace = {
-      {
-        'BufReadPost',
-        '*',
-        [[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]],
-      },
-    },
-    neotree = {
-      {
-        'VimEnter',
-        '*',
-        [[if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in") | exe 'lua dynamo_file_explorer()' | wincmd p | ene | exe 'cd '.argv()[0] | endif]],
-      },
-    },
+  vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+    pattern = { '*' },
+    group = vim.api.nvim_create_augroup('mkdir_saving', {}),
+    callback = function()
+      dynamo_mkdir_saving()
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+    pattern = { '*' },
+    group = vim.api.nvim_create_augroup('terminal', {}),
+    callback = function()
+      dynamo_setft_terminal()
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ 'FileType' }, {
+    pattern = { 'qf,help,man,lspinfo,lsp-installer' },
+    group = vim.api.nvim_create_augroup('quick_quit_manual', {}),
+    command = 'nnoremap <silent> <buffer> q :close<CR>',
+  })
+
+  vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
+    pattern = { '*' },
+    group = vim.api.nvim_create_augroup('lastplace', {}),
+    command = [[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]],
+  })
+
+  vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+    pattern = { '*' },
+    group = vim.api.nvim_create_augroup('neotree', {}),
+    command = [[if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in") | exe 'lua dynamo_file_explorer()' | wincmd p | ene | exe 'cd '.argv()[0] | endif]],
   })
 end
 
 M.enable_highlight_document = function(client_id)
-  M.create_augroups({
-    highlight_lsp = {
-      {
-        'CursorHold',
-        '<buffer>',
-        string.format('lua dynamo_lsp_document_highlight(%d)', client_id),
-      },
-      { 'CursorMoved', '<buffer>', 'lua vim.lsp.buf.clear_references()' },
-    },
-  }, true, client_id)
+  local id = vim.api.nvim_create_augroup('highlight_lsp', {})
+
+  vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+    buffer = 0,
+    group = id,
+    callback = function()
+      dynamo_lsp_document_highlight(client_id)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ 'CursorMoved' }, {
+    buffer = 0,
+    group = id,
+    callback = function()
+      vim.lsp.buf.clear_references()
+    end,
+  })
 end
 
 M.disable_highlight_document = function()
-  M.delete_augroups('highlight_lsp')
+  vim.api.nvim_del_augroup_by_name('highlight_lsp')
 end
 
 M.enable_codelens = function(client_id)
   dynamo_lsp_codelens(client_id)
-  M.create_augroups({
-    codelens_lsp = {
-      {
-        'TextChanged,InsertLeave',
-        '<buffer>',
-        string.format('lua dynamo_lsp_codelens(%d)', client_id),
-      },
-    },
-  }, true, client_id)
+
+  vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave' }, {
+    buffer = 0,
+    group = vim.api.nvim_create_augroup('codelens_lsp', {}),
+    callback = function()
+      dynamo_lsp_codelens(client_id)
+    end,
+  })
 end
 
 M.disable_codelens = function()
-  M.delete_augroups('codelens_lsp')
+  vim.api.nvim_del_augroup_by_name('codelens_lsp')
 end
 
 M.enable_hover = function(client_id)
-  M.create_augroups({
-    hover_lsp = {
-      {
-        'CursorHold',
-        '<buffer>',
-        string.format('lua dynamo_lsp_hover(%d)', client_id),
-      },
-    },
-  }, true, client_id)
+  vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+    buffer = 0,
+    group = vim.api.nvim_create_augroup('hover_lsp', {}),
+    callback = function()
+      dynamo_lsp_hover(client_id)
+    end,
+  })
 end
 
 M.disable_hover = function()
-  M.delete_augroups('hover_lsp')
+  vim.api.nvim_del_augroup_by_name('hover_lsp')
 end
 
 M.enable_codeaction = function(client_id)
-  M.create_augroups({
-    codeaction_lsp = {
-      {
-        'CursorHold,CursorHoldI',
-        '<buffer>',
-        string.format('lua dynamo_lsp_codeaction(%d)', client_id),
-      },
-    },
-  }, true, client_id)
+  vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+    buffer = 0,
+    group = vim.api.nvim_create_augroup('codeaction_lsp', {}),
+    callback = function()
+      dynamo_lsp_codeaction(client_id)
+    end,
+  })
 end
 
 M.disable_codeaction = function()
-  M.delete_augroups('codeaction_lsp')
+  vim.api.nvim_del_augroup_by_name('codeaction_lsp')
 end
 
 M.enable_formatting = function(client_id)
-  M.create_augroups({
-    formatting_lsp = {
-      {
-        'BufWritePre',
-        '<buffer>',
-        string.format('lua dynamo_lsp_formatting(%d)', client_id),
-      },
-    },
-  }, true, client_id)
+  vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+    buffer = 0,
+    group = vim.api.nvim_create_augroup('formatting_lsp', {}),
+    callback = function()
+      dynamo_lsp_formatting(client_id)
+    end,
+  })
 end
 
 M.disable_formatting = function()
-  M.delete_augroups('formatting_lsp')
+  vim.api.nvim_del_augroup_by_name('formatting_lsp')
 end
 
 M.auto_install_ts_parser = function()
-  M.create_augroups({
-    install_ts_parser = {
-      {
-        'FileType',
-        '*',
-        'lua dynamo_ts_install()',
-      },
-    },
+  vim.api.nvim_create_autocmd({ 'FileType' }, {
+    pattern = { '*' },
+    group = vim.api.nvim_create_augroup('ts_parser_manager', {}),
+    callback = function()
+      dynamo_ts_install()
+    end,
   })
 end
 
