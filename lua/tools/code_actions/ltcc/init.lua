6f0730fc7c22555bb80677515a929a1fda35cdef
Trần Đức Nam <dynamo.foss@gmail.com>
2023-05-12 19:22:27 +0700

    feat: add `ltcc` support for bash script

---
+++ lua/tools/code_actions/ltcc/init.lua
@@ -0,0 +1,25 @@
+local h = require('null-ls.helpers')
+local methods = require('null-ls.methods')
+
+return h.make_builtin({
+  name = 'ltcc',
+  meta = {
+    description = 'My custom sources to use languagetool-code-comments',
+    url = 'https://github.com/dynamotn/languagetool-code-comments',
+  },
+  method = methods.internal.CODE_ACTION,
+  filetypes = {},
+  generator_opts = {
+    command = 'ltcc',
+    args = { 'check', '-l', 'en-US', '-f', '$FILENAME' },
+    format = 'json',
+    to_stdin = true,
+    timeout = 60000,
+    check_exit_code = function(c)
+      return c <= 1
+    end,
+    on_output = require('tools.code_actions.ltcc.executor').handle_output,
+    use_cache = false,
+  },
+  factory = h.generator_factory,
+})
