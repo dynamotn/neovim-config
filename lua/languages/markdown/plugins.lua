4827e6ee221ed425c4624c04b7fbda5a15b1f9bc
Trần Đức Nam <dynamo.foss@gmail.com>
2023-06-11 19:45:15 +0700

    chore: update version of plugins

--- lua/languages/markdown/plugins.lua
+++ lua/languages/markdown/plugins.lua
@@ -5,7 +5,7 @@ return {
         name = 'markdown_preview',
         build = 'cd app && npm install',
       }
-    or nil,
+    or {},
   { 'SidOfc/mkdx', name = 'mkdx' }, -- Handle lists, checkboxes, fenced code blocks, shortcuts, headers and links...
   { 'dhruvasagar/vim-table-mode' }, -- Formatting
 }
