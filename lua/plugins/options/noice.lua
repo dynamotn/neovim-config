1eea2cad3fb2bc5dadc25e76ecf9b2a127e5f5d6
Trần Đức Nam <dynamo.foss@gmail.com>
2023-07-20 11:09:34 +0700

    perf: ignore notify when hover text

--- lua/plugins/options/noice.lua
+++ lua/plugins/options/noice.lua
@@ -16,4 +16,15 @@ noice.setup({
   presets = {
     lsp_doc_border = true,
   },
+  routes = {
+    {
+      filter = {
+        event = 'notify',
+        find = 'No information available',
+      },
+      opts = {
+        skip = true,
+      },
+    },
+  },
 })
