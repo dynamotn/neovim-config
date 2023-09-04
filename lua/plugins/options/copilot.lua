a9410a50b68b97bdde80f4058165a17d5ae11e9d
Trần Đức Nam <dynamo.foss@gmail.com>
2023-09-04 16:51:57 +0700

    refactor: setup copilot

--- lua/plugins/options/copilot.lua
+++ lua/plugins/options/copilot.lua
@@ -1,3 +1,25 @@
 local copilot = require('copilot')
 
-copilot.setup({})
+copilot.setup({
+  panel = { enabled = false },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept = '<C-CR>',
+      accept_word = false,
+      accept_line = '<C-Space>',
+      next = '<C-n>',
+      prev = '<C-p>',
+      dismiss = '<C-c>',
+    },
+  },
+  server_opts_overrides = {
+    settings = {
+      advanced = {
+        inlineSuggestCount = 5,
+      },
+    },
+  },
+})
