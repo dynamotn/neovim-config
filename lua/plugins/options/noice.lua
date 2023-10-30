f880fd4613532a57d6bd0d07d116cf80b277362b
Trần Đức Nam <dynamo.foss@gmail.com>
2023-10-31 06:43:08 +0700

    feat: noice for documentation of cmp

--- lua/plugins/options/noice.lua
+++ lua/plugins/options/noice.lua
@@ -10,7 +10,7 @@ noice.setup({
     override = {
       ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
       ['vim.lsp.util.stylize_markdown'] = true,
-      ['cmp.entry.get_documentation'] = false,
+      ['cmp.entry.get_documentation'] = true,
     },
     message = {
       view = 'mini',
