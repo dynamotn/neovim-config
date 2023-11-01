84b48c650c985a3f4fe25108ba74a800cdecdd25
Trần Đức Nam <dynamo.foss@gmail.com>
2023-11-01 23:28:03 +0700

    refactor: change gotemplate autopairs

--- lua/languages/gotmpl/autopairs.lua
+++ lua/languages/gotmpl/autopairs.lua
@@ -1,6 +1,11 @@
 return function(filetypes, rule)
   return {
     -- Add parentheses in function
-    rule('{', '{  }}', filetypes):set_end_pair_length(3),
+    rule('{{', '  }', filetypes):set_end_pair_length(2),
+    rule('{-', '{-  }', filetypes)
+      :replace_endpair(function(_)
+        return '<BS><BS>{{-  }'
+      end)
+      :set_end_pair_length(2),
   }
 end
