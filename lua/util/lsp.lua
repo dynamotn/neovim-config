8151c9e26a8915106487c312e738536058fcaec9
Trần Đức Nam <me@dynamotn.dev>
2024-04-21 18:42:34 +0700

    feat: lsp when rename file on neotree

--- lua/util/lsp.lua
+++ lua/util/lsp.lua
@@ -20,4 +20,40 @@ M.formatting = function()
   end)
 end
 
+-- List LSP clients that support specific method
+function M.get_clients(opts)
+  local ret = {}
+  if vim.lsp.get_clients then
+    ret = vim.lsp.get_clients(opts)
+  else
+    ret = vim.lsp.get_active_clients(opts)
+    if opts and opts.method then
+      ret = vim.tbl_filter(function(client)
+        return client.supports_method(opts.method, { bufnr = opts.bufnr })
+      end, ret)
+    end
+  end
+  return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
+end
+
+-- Update LSP when rename file
+M.on_rename = function(from, to)
+  local clients = M.get_clients()
+  for _, client in ipairs(clients) do
+    if client.supports_method("workspace/willRenameFiles") then
+      local resp = client.request_sync("workspace/willRenameFiles", {
+        files = {
+          {
+            oldUri = vim.uri_from_fname(from),
+            newUri = vim.uri_from_fname(to),
+          },
+        },
+      }, 1000, 0)
+      if resp and resp.result ~= nil then
+        vim.lsp.util.apply_workspace_edit(resp.result, client.offset_encoding)
+      end
+    end
+  end
+end
+
 return M
