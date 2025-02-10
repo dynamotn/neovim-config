91b029857a6f4200823b5bd932c543ebf788dcdb
Trần Đức Nam <me@dynamotn.dev>
2025-02-10 17:33:55 +0700

    refactor: change to use new convention of function name in bash

--- lua/languages/bash/autopairs.lua
+++ lua/languages/bash/autopairs.lua
@@ -1,6 +1,6 @@
 return function(filetypes, rule)
   return {
     -- Add parentheses in function
-    rule('^_.*%(%)$', ' { }', filetypes):use_regex(true):set_end_pair_length(0),
+    rule('^.*%(%)$', ' { }', filetypes):use_regex(true):set_end_pair_length(0),
   }
 end
