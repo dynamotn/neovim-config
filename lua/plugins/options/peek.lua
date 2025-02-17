54f864695aa1716ec6f21e20724b8f54367e4a40
Trần Đức Nam <me@dynamotn.dev>
2025-02-17 16:28:28 +0700

    refactor: use default browser for peek

--- lua/plugins/options/peek.lua
+++ lua/plugins/options/peek.lua
@@ -1,20 +1,2 @@
 local peek = require('peek')
-local job = require('plenary.job')
-
-local opts = {}
-
-job
-  :new({
-    command = 'grep',
-    args = { '-q', 'Gentoo', '/etc/os-release' },
-    on_exit = function(_, return_val)
-      if return_val == 0 then
-        opts.app = 'firefox-bin'
-      else
-        opts.app = 'firefox'
-      end
-
-      peek.setup(opts)
-    end,
-  })
-  :start()
+peek.setup()
