8e47aca74cd58a8d2ca9cf385028175b4726e1ba
Trần Đức Nam <me@dynamotn.dev>
2025-03-12 11:06:46 +0700

    refactor: unified tool lua files

--- lua/tools/code_actions/ltcc/init.lua
+++ lua/tools/code_actions/ltcc/init.lua
@@ -1,6 +1,8 @@
 local h = require('null-ls.helpers')
 local methods = require('null-ls.methods')
 
+local neoconf = require('neoconf')
+
 return h.make_builtin({
   name = 'ltcc',
   meta = {
@@ -16,7 +18,40 @@ return h.make_builtin({
     to_stdin = true,
     timeout = 60000,
     check_exit_code = function(c) return c <= 1 end,
-    on_output = require('tools.code_actions.ltcc.executor').handle_output,
+    on_output = function(params)
+      local actions = {}
+
+      for _, m in ipairs(params.output) do
+        if
+          m.replacements ~= vim.NIL
+          and params.row == m.moreContext.line_number
+        then
+          local row = m.moreContext.line_number - 1
+          local col_beg = m.moreContext.line_offset
+          local col_end = m.moreContext.line_offset + m.length
+
+          for _, r in ipairs(m.replacements) do
+            if string.find(r.value, 'not shown') == nil then
+              table.insert(actions, {
+                title = 'Replace with “' .. r.value .. '”',
+                action = function()
+                  vim.api.nvim_buf_set_text(
+                    params.bufnr,
+                    row,
+                    col_beg,
+                    row,
+                    col_end,
+                    { r.value }
+                  )
+                end,
+              })
+            end
+          end
+        end
+      end
+
+      return actions
+    end,
     use_cache = false,
   },
   factory = h.generator_factory,
--- lua/tools/diagnostics/ltcc/init.lua
+++ lua/tools/diagnostics/ltcc/init.lua
@@ -16,7 +16,42 @@ return h.make_builtin({
     to_stdin = true,
     timeout = 60000,
     check_exit_code = function(c) return c <= 1 end,
-    on_output = require('tools.diagnostics.ltcc.executor').handle_output,
+    on_output = function(params)
+      local file = params.output
+      if file then
+        local parser = h.diagnostics.from_json({
+          severities = {
+            ERROR = h.diagnostics.severities.error,
+          },
+        })
+
+        local offenses = {}
+
+        for _, m in ipairs(file) do
+          local tip = table.concat(
+            vim.tbl_map(
+              function(r) return '“' .. r.value .. '”' end,
+              m.replacements
+            ),
+            ', '
+          )
+
+          table.insert(offenses, {
+            message = m.message .. ' Try: ' .. tip,
+            ruleId = m.rule.id,
+            level = 'ERROR',
+            line = m.moreContext.line_number,
+            column = m.moreContext.line_offset + 1,
+            endLine = m.moreContext.line_number,
+            endColumn = m.moreContext.line_offset + m.length + 1,
+          })
+        end
+
+        return parser({ output = offenses })
+      end
+
+      return {}
+    end,
     use_cache = false,
   },
   factory = h.generator_factory,
