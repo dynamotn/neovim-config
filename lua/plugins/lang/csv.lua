ffe84a0671c2c41b0ae74d53266518d09e807c1f
Trần Đức Nam <me@dynamotn.dev>
2025-06-28 23:23:13 +0700

    feat(csv): new plugin for Navigation and table view

--- lua/plugins/lang/csv.lua
+++ lua/plugins/lang/csv.lua
@@ -3,8 +3,8 @@ local language = require('config.languages').csv
 return vim.list_contains(_G.enabled_languages, 'csv')
     and {
       {
-        -- Navigation in file
-        'dynamotn/csv.vim',
+        -- Navigation and table view
+        'hat0uma/csvview.nvim',
         ft = language.filetypes,
       },
     }
