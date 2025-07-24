fe2530954f8e3980aeb887ea86943dce68be7068
Trần Đức Nam <me@dynamotn.dev>
2025-07-24 11:03:28 +0700

    feat: add devcontainer support

--- lua/plugins/toolbox/project.lua
+++ lua/plugins/toolbox/project.lua
@@ -1,4 +1,16 @@
 return {
   -- Project setting
   { import = 'lazyvim.plugins.extras.lsp.neoconf' },
+  {
+    -- Devcontainer
+    'https://codeberg.org/esensar/nvim-dev-container',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      opts = function(_, opts)
+        opts.ensure_installed = {
+          'jsonc', -- for devcontainer.json
+        }
+      end,
+    },
+  },
 }
