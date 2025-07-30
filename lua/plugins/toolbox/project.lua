84b3da491057fd1e63617458e1ccd737828dae4b
Trần Đức Nam <me@dynamotn.dev>
2025-07-30 17:27:42 +0700

    feat: add other-nvim for alternative files

--- lua/plugins/toolbox/project.lua
+++ lua/plugins/toolbox/project.lua
@@ -2,6 +2,45 @@ return {
   -- Project setting
   { import = 'lazyvim.plugins.extras.lsp.neoconf' },
   {
+    -- Intergrate project management with which-key
+    'folke/which-key.nvim',
+    opts = {
+      spec = {
+        { '<leader>p', group = 'project' },
+      },
+    },
+  },
+  {
+    -- Open alternative files in the project
+    'rgroli/other.nvim',
+    main = 'other-nvim',
+    lazy = false,
+    opts = {
+      mappings = {
+        'angular',
+        'laravel',
+        'rails',
+        'golang',
+        'python',
+        'react',
+        'rust',
+        {
+          pattern = '/src/(.*).sh$',
+          target = '/test/%1.bats',
+          context = 'test',
+        },
+      },
+    },
+    keys = {
+      {
+        '<leader>pa',
+        function() require('other-nvim').openVSplit() end,
+        desc = 'Open alternative file',
+        mode = { 'n', 't' },
+      },
+    },
+  },
+  {
     -- Devcontainer
     'https://codeberg.org/esensar/nvim-dev-container',
     dependencies = {
