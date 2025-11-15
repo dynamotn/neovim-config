97c6df87c5e937f0603a86b9f012930b69449f79
Trần Đức Nam <me@dynamotn.dev>
2025-11-15 23:59:02 +0700

    refactor: switch from render-markdown to markview

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -36,39 +36,13 @@ return vim.list_contains(_G.enabled_languages, 'markdown')
         },
       },
       {
-        -- Highlight for headlines, codeblocks
-        'MeanderingProgrammer/render-markdown.nvim',
-        ft = language.filetypes,
-        opts = {
-          code = {
-            sign = false,
-            width = 'block',
-            right_pad = 1,
-          },
-          heading = {
-            sign = false,
-            icons = {},
-          },
-          checkbox = {
-            enabled = false,
-          },
-        },
-        config = function(_, opts)
-          require('render-markdown').setup(opts)
-          Snacks.toggle({
-            name = 'Render Markdown',
-            get = require('render-markdown').get,
-            set = require('render-markdown').set,
-          }):map('<leader>um')
-        end,
-      },
-      {
         -- Obsidian
         'obsidian-nvim/obsidian.nvim',
         ft = language.filetypes,
         enabled = _G.used_full_plugins or _G.enabled_plugins.obsidian,
         opts = {
           workspaces = _G.obsidian.vaults(),
+          ui = { enable = false },
         },
         init = function()
           _G.completion_sources =
--- lua/plugins/ui/highlight.lua
+++ lua/plugins/ui/highlight.lua
@@ -1,3 +1,10 @@
+local languages_list = require('config.languages')
+local markview_filetypes = {}
+vim.list_extend(markview_filetypes, languages_list.markdown.filetypes)
+vim.list_extend(markview_filetypes, languages_list.html.filetypes)
+vim.list_extend(markview_filetypes, languages_list.typst.filetypes)
+vim.list_extend(markview_filetypes, languages_list.yaml.filetypes)
+
 return {
   -- Highlight patterns (colors)
   { import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
@@ -19,5 +26,33 @@ return {
       }
     end,
     main = 'rainbow-delimiters.setup',
+  },
+  {
+    -- Syntax highlight
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+    opts = {
+      preview = {
+        icon_provider = 'devicons',
+        enable = true,
+      },
+      html = {
+        enable = true,
+      },
+      typst = {
+        enable = true,
+      },
+      yaml = {
+        enable = true,
+      },
+    },
+    config = function(opts)
+      local presets = require('markview.presets')
+      require('markview').setup(vim.tbl_deep_extend('force', opts, {
+        markdown = {
+          headings = presets.headings.glow,
+        },
+      }))
+    end,
   },
 }
